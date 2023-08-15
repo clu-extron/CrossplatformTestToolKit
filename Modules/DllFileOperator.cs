6ca7c662cd2c3fd679f18cc1ec4abd356bdadb91
Chaoran Lu <clu@extron.com>
2023-08-14 17:49:36 -0700

    Add project files.

---
+++ Modules/DllFileOperator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossplatformTestToolKit.Modules
+{
+    public class DllFileOperator
+    {
+        /// <summary>
+        /// This PUBLIC method check folder A and folder B and compare the dll file name to look for the ones that only exist in folder A but missing in folder B
+        /// </summary>
+        /// <param name="folderA"></param>
+        /// <param name="folderB"></param>
+        public void DllExistInFolderAButNotInFolderB(string folderA, string folderB)
+        {
+            List<string> filesInA = GetDllFiles(folderA);
+            List<string> filesInB = GetDllFiles(folderB);
+
+            Console.WriteLine("DLL files present in A but not in B: ");
+            int count = 0;
+
+            foreach (string file in filesInA.Except(filesInB))
+            {
+                Console.WriteLine(file);
+                count++;
+            }
+            Console.WriteLine("Result: There are " + count + " DLL files that present in A but not in B");
+        }
+
+        /// <summary>
+        /// This PRIVATE method check a directory and returns a list of dll files under it.
+        /// </summary>
+        /// <param name="folderPath"></param>
+        /// <returns>list of dll file name string</returns>
+        private List<string> GetDllFiles(string folderPath)
+        {
+            List<string> dllFiles = new List<string>();
+            string[] files = Directory.GetFiles(folderPath, "*.dll");
+            foreach (string file in files)
+            {
+                dllFiles.Add(Path.GetFileName(file));
+            }
+            return dllFiles;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using CrossplatformTestToolKit.Modules;
+
+DllFileOperator testOperator = new DllFileOperator();
+string pathA = @"C:\Users\clu\Desktop\Extron Room Agent - Beta";
+string pathB = @"C:\Users\clu\Desktop\Extron Room Agent";
+testOperator.DllExistInFolderAButNotInFolderB(pathA, pathB);
