edda0743cc88bca60c7cfc91c1631d32c3cc0c51
Chaoran Lu <clu@extron.com>
2024-01-10 16:47:18 -0800

    Add a new module to count data in a resx translation file

---
+++ Modules/ResxInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CrossplatformTestToolKit.Modules
+{
+    public class ResxInfo
+    {
+        public int GetDataCount(string path)
+        {
+            string resFilePath = path;
+            int count = 0;
+            using (XmlReader reader = XmlReader.Create(path))
+            {
+                while (reader.Read())
+                {
+                    // Looking for the start of an element named "data"
+                    if (reader.NodeType == XmlNodeType.Element && reader.Name == "data")
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿using CrossplatformTestToolKit.Modules;
 
-DllFileOperator testOperator = new DllFileOperator();
-string pathA = @"C:\Users\clu\Desktop\Extron Room Agent - Beta";
-string pathB = @"C:\Users\clu\Desktop\Extron Room Agent";
-testOperator.DllExistInFolderAButNotInFolderB(pathA, pathB);
+// DllFileOperator Example
+//DllFileOperator testOperator = new DllFileOperator();
+//string pathA = @"C:\Users\clu\Desktop\Debug";
+//string pathB = @"C:\Users\clu\Desktop\Release";
+//testOperator.DllExistInFolderAButNotInFolderB(pathA, pathB);
+
+ResxInfo test = new ResxInfo();
+int result = test.GetDataCount("C:\\Project\\clu_CLU-LT_PCS.Modules_Dev_NewRelease\\PCS.Modules\\ShareLinkFamily\\Source\\EAF.Modules.ShareLinkFamily\\Properties\\Resources.zh-Hans.resx");
+Console.WriteLine(result);
